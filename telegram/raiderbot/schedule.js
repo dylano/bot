be8e60ddcca580d8fe95af685462098fca6b3366
Dylan Oliver <d@doliver.net>
2018-08-27 22:02:25 -0700

    full schedule

--- telegram/raiderbot/schedule.js
+++ telegram/raiderbot/schedule.js
@@ -1,13 +1,23 @@
 const getSchedule = () => {
   return `
   <em>2018 Schedule</em>
-    <b>9/10: Rams</b>
+    <b>9/10: Rams</b> <i>(MNF)</i>
     9/16: @ Broncos
     9/23: @ Dolphins
     <b>9/30: Browns</b>
     10/7: @ Chargers
-    10/14: @ London
-    10/21: BYE
+    10/14: Seahawks <i>(London)</i>
+    10/21: <i>BYE</i>
+    <b>10/28: Colts</b>
+    11/1: @ SF <i>(Thurs)</i>
+    11/11: <b>Chargers</b>
+    11/18: @ Cardinals
+    11/25: @ Baltimore
+    12/2: <b>Chiefs</b>
+    12/9: <b>Steelers</b> <i>(SNF)</i>
+    12/16: @ Bengals
+    12/24: <b>Broncos</b> <i>(MNF)</i>
+    12/30: @ Chiefs
     `;
 };
 
